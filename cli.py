43e45caac74c161fa8d7eaef325e9ea11d658e24
Wangwei <69316500+wangweik@users.noreply.github.com>
2022-07-29 17:25:39 +0100

    add_input_output_abs_path

--- cli.py
+++ cli.py
@@ -2,6 +2,7 @@
 
 import click
 import json
+import os
 
 
 from engines.screening_model import run_main_screening 
@@ -12,18 +13,20 @@ from engines.investment_model import run_main_investment
 def cli():
     pass
 
-
+cli_file_path = os.path.abspath(os.path.dirname(__file__))
+inputDir = cli_file_path + '\\engines'
+outputDir = cli_file_path + '\\engines\\results'
 
 
 # TODO: Add default files names
 @click.command(help='Run the screening model') 
-@click.option('--input_dir',                 prompt='input directory:',   default="engines",                    help='Specify the directory for input data')
-@click.option('--output_dir',                prompt='output dirctory:',   default="engines\\results",           help='Specify the directory for output data')
+@click.option('--input_dir',                 prompt='input directory:',   default=inputDir,                     help='Specify the directory for input data')
+@click.option('--output_dir',                prompt='output dirctory:',   default=outputDir,                    help='Specify the directory for output data')
 @click.option('--country',                   prompt='country name:',      default='UK',                         help='Specify the country:"PT", "HR", or "UK"')
 @click.option('--test_case',                 prompt='test case name:',    default='case5',                      help='Specify the test_case witout ".m". ')
-@click.option('--xlsx_file_name',            prompt='time-series data:',  default=' ',                          help='Specify the xlsx_file_name witout ".xlsx". ')
-@click.option('--ods_file_name',             prompt='contingency data:',  default=' ',                          help='Specify the ods_file_name witout ".ods". ')
-@click.option('--peak_hour',                 prompt='peak_hour:',         default=19,                           help='Specify the number between 1 and 24, i.e., 19 for 7 p.m. ')
+@click.option('--xlsx_file_name',            prompt='time-series data:',  default='',                           help='Specify the xlsx_file_name witout ".xlsx". ')
+@click.option('--ods_file_name',             prompt='contingency data:',  default='case5_bus_python_test',      help='Specify the ods_file_name witout ".ods". ')
+@click.option('--peak_hour',                 prompt='peak_hour:',         default= 19,                          help='Specify the number between 1 and 24, i.e., 19 for 7 p.m. ')
 @click.option('--no_year',                   prompt='Number of years:',   default= 4,                           help='Specify the number of years: 1 - [2020], 2 - [2020,2030], 3 - [2020,2030,2040], 4 - [2020,2030,2040,2050]')
 
 def run_screening(input_dir, output_dir,ods_file_name, xlsx_file_name, country, test_case, peak_hour,no_year): 
@@ -35,8 +38,8 @@ def run_screening(input_dir, output_dir,ods_file_name, xlsx_file_name, country, 
 
 
 @click.command(help='Run the investment model') 
-@click.option('--input_dir',                 prompt='input directory:',   default="C:/Users/p96677wk/Dropbox (The University of Manchester)/My PC (E-10LPC1N2L4S)/Desktop/ATTEST/Tool3.2/ATTEST_Tool3.2/engines",                  help='Specify the directory for input data')
-@click.option('--output_dir',                prompt='output dirctory:',   default="C:/Users/p96677wk/Dropbox (The University of Manchester)/My PC (E-10LPC1N2L4S)/Desktop/ATTEST/Tool3.2/ATTEST_Tool3.2/engines/results",           help='Specify the directory for output data')
+@click.option('--input_dir',                 prompt='input directory:',   default=inputDir,                     help='Specify the directory for input data')
+@click.option('--output_dir',                prompt='output dirctory:',   default=outputDir,                    help='Specify the directory for output data')
 @click.option('--country',                   prompt='country name:',      default='UK',                         help='Specify the country:"PT", "HR", or "UK"')
 @click.option('--test_case',                 prompt='test case name:',    default='case5',                      help='Specify the test_case witout ".m". ')
 @click.option('--xlsx_file_name',            prompt='time-series data:',  default='',                           help='Specify the xlsx_file_name witout ".xlsx". ')
@@ -54,8 +57,8 @@ def run_investment(input_dir, output_dir, ods_file_name, xlsx_file_name, country
 
 
 @click.command(help='Run all models') 
-@click.option('--input_dir',                 prompt='input directory:',   default="C:/Users/p96677wk/Dropbox (The University of Manchester)/My PC (E-10LPC1N2L4S)/Desktop/ATTEST/Tool3.2/ATTEST_Tool3.2/engines",                  help='Specify the directory for input data')
-@click.option('--output_dir',                prompt='output dirctory:',   default="C:/Users/p96677wk/Dropbox (The University of Manchester)/My PC (E-10LPC1N2L4S)/Desktop/ATTEST/Tool3.2/ATTEST_Tool3.2/engines/results",           help='Specify the directory for output data')
+@click.option('--input_dir',                 prompt='input directory:',   default=inputDir,                     help='Specify the directory for input data')
+@click.option('--output_dir',                prompt='output dirctory:',   default=outputDir,                    help='Specify the directory for output data')
 @click.option('--country',                   prompt='country name:',      default='UK',                         help='Specify the country:"PT", "HR", or "UK"')
 @click.option('--test_case',                 prompt='test case name:',    default='case5',                      help='Specify the test_case witout ".m". ')
 @click.option('--xlsx_file_name',            prompt='time-series data:',  default='',                           help='Specify the xlsx_file_name witout ".xlsx". ')
